89d6404a2592d6c6e1a713cf05a0aba2d40ff684
Charles S <charlessoaresdossantos@gmail.com>
2020-01-15 09:36:35 -0300

    Project created.

---
+++ src/test/java/com/charlessantos/cardeal/CardealApplicationTests.java
@@ -0,0 +1,13 @@
+package com.charlessantos.cardeal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardealApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
