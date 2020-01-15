471e8f8531c167e256bbf480d1d3cb0dab9c9878
Charles S <charlessoaresdossantos@gmail.com>
2020-01-15 20:04:33 -0300

    Implemented start up method to populate categories

--- src/main/java/com/charlessantos/cardeal/CardealApplication.java
+++ src/main/java/com/charlessantos/cardeal/CardealApplication.java
@@ -1,13 +1,28 @@
 package com.charlessantos.cardeal;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CardealApplication {
+import com.charlessantos.cardeal.domain.Category;
+import com.charlessantos.cardeal.repositories.CategoryRepository;
 
+@SpringBootApplication
+public class CardealApplication implements CommandLineRunner {
+
+	@Autowired
+	CategoryRepository categoryRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(CardealApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		categoryRepository.saveAll(Arrays.asList(new Category(null, "SUV"), new Category(null, "Hatch"), new Category(null, "Sedan")));
+		
 	}
 
 }
