4ec9f960c0415f6ffe231cbe384843189c24ffcc
Charles S <charlessoaresdossantos@gmail.com>
2020-01-25 00:12:18 -0300

    Implemented a custom validation for client email

--- src/main/java/com/charlessantos/cardeal/repositories/ClientRepository.java
+++ src/main/java/com/charlessantos/cardeal/repositories/ClientRepository.java
@@ -2,10 +2,13 @@ package com.charlessantos.cardeal.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.charlessantos.cardeal.domain.Client;
 
 @Repository
 public interface ClientRepository extends JpaRepository<Client, Integer> {
 
+	@Transactional(readOnly=true)
+	public Client findByEmail(String email);
 }
--- src/main/java/com/charlessantos/cardeal/services/validation/ClientInsertValidator.java
+++ src/main/java/com/charlessantos/cardeal/services/validation/ClientInsertValidator.java
@@ -6,12 +6,19 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.charlessantos.cardeal.domain.Client;
 import com.charlessantos.cardeal.domain.enums.TypeClient;
 import com.charlessantos.cardeal.dto.ClientDTO;
+import com.charlessantos.cardeal.repositories.ClientRepository;
 import com.charlessantos.cardeal.resources.exception.FieldMessage;
 import com.charlessantos.cardeal.services.validation.utils.BR;
 
 public class ClientInsertValidator implements ConstraintValidator<ClientInsert, ClientDTO> {
+	@Autowired
+	ClientRepository clientRepo;
+
 	@Override
 	public void initialize(ClientInsert ann) {
 	}
@@ -27,6 +34,12 @@ public class ClientInsertValidator implements ConstraintValidator<ClientInsert, 
 		if (objDto.getTypeClient().equals(TypeClient.PHYSICALPERSON.getCode())
 				&& !BR.isValidCNPJ(objDto.getCpfOrCnpj())) {
 			list.add(new FieldMessage("cpfOrCnpj", "CPF inválido!"));
+		}
+
+		Client client = clientRepo.findByEmail(objDto.getEmail());
+
+		if(client != null) {
+			list.add(new FieldMessage("email", "Este e-mail já existe."));
 		}
 
 		for (FieldMessage e : list) {
