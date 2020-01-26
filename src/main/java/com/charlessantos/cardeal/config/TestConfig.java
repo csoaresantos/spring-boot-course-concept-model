15803301a57eb5849c89b01c28e01384287f7bb7
Charles S <charlessoaresdossantos@gmail.com>
2020-01-25 23:05:03 -0300

    Created a test profile

---
+++ src/main/java/com/charlessantos/cardeal/config/TestConfig.java
@@ -0,0 +1,24 @@
+package com.charlessantos.cardeal.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.charlessantos.cardeal.services.DBService;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+
+	@Autowired
+	DBService dbService;
+	
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+		dbService.instantiateTestDatabase();
+		return true;
+	}
+}
