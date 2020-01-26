68ab40ab028eb99bb31a45985dc29a02cdc2455f
Charles S <charlessoaresdossantos@gmail.com>
2020-01-26 01:12:46 -0300

    added implementation of heroku server

---
+++ src/main/java/com/charlessantos/cardeal/config/DevConfig.java
@@ -0,0 +1,32 @@
+package com.charlessantos.cardeal.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.charlessantos.cardeal.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+	@Autowired
+	DBService dbService;
+
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+		
+		if(!"create".equals(strategy))
+			return false;
+
+		dbService.instantiateTestDatabase();
+		return true;
+	}
+}
